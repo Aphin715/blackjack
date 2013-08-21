12f3b44e3e9c9b8181ac025cac65643ccad4691f
Alex Phinizy <AlexPhinizy@gmail.com>
2013-08-21 19:37:05 -0400

    Player turn complete

--- blackjack.rb
+++ blackjack.rb
@@ -39,6 +39,52 @@ def initial_deal(player)
   puts player.hand
 end
 
+def prompt
+  print "Hit or stand (H/S)"
+  gets.chomp
+end
+
+def validate(player)
+  until (input = prompt.upcase) == "S"
+    if input == "H"
+      if !hit(player)
+        break
+      end
+    else
+      puts "invalid input"
+    end
+  end
+  puts "starndiingasd"
+end
+
+def hit(player)
+  deal(player)
+  display_score(player)
+end
+
+def display_score(player)
+  score = 0
+  player.hand.each do |card|
+    if card.chop == "J" || card.chop == "Q" || card.chop == "K"
+      score += 10
+    elsif card.chop != "A"
+      score += card.chop.to_i
+    else
+      if score < 11
+        score += 11
+      else
+        score += 1
+      end
+    end
+  end
+  if score > 21
+    puts "BUST! YOU'RE A LOSER"
+    false
+  else
+    puts score
+    true
+  end
+end
 
 
 # Player's score is shown before each hit or stand
@@ -56,3 +102,4 @@ end
 
 game = Blackjack.new
 game.initial_deal(game.player)
+game.validate(game.player)
