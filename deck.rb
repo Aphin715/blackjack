2b981d2d8cfefb3bb14e19a9d7411a50bb8e9e3a
Michael McLellan <jmikem825@gmail.com>
2013-08-26 17:21:14 -0400

    Re-extract build _deck method

--- deck.rb
+++ deck.rb
@@ -5,15 +5,19 @@ class Deck
   VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
   def initialize
-    @deck = []
+    @deck = build_deck
+  end
+
+  def build_deck
+    deck = []
 
     SUITS.each do |suit|
       VALUES.each do |value|
-        @deck.push(Card.new(value, suit))
+        deck.push(Card.new(value, suit))
       end
     end
 
-    @deck.shuffle!
+    deck.shuffle
   end
 
   def pop
